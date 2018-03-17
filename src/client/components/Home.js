cac418412fcdec7dd873cd42b4c711684a67ad62
Łukasz Gorzelany <gorzelany.lukasz@gmail.com>
2018-03-17 14:57:54 +0100

    add react-router

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <div>I am the home component</div>
+      <div>I am the home dsa</div>
       <button onClick={() => console.log("click me")}>Click me</button>
     </div>
   );
